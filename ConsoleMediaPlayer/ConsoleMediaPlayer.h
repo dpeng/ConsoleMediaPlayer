91af7dde8d6d9361067f00bcd7bdcb82e95d2cbc
dpeng <df.peng@hotmail.com>
2019-01-15 17:25:31 +0800

    add console control by key and mouse

--- ConsoleMediaPlayer/ConsoleMediaPlayer.h
+++ ConsoleMediaPlayer/ConsoleMediaPlayer.h
@@ -12,12 +12,18 @@ extern "C"
 #include <progressbar/progressbar.h>
 }
 
-
+#define MAX_SONGLENGTH			260
+#define MAX_MULTISONGLENGTH		1600
 progressbar *m_pProgressBar = NULL;
 vector<char*> filenameVector;
 int curPlayingIndex;
 SDL_TimerID progressBarTimerID = 0;
-
+HANDLE consoleInputHandler;
+HANDLE consoleOutputHandler;
+HANDLE playProcessHandler;
+HANDLE consoleProcessHandler;
+int consoleWidth = 0;
+BOOL bIsPlaying;
 
 int CMP_Play(void);
 int CMP_Stop(void);
