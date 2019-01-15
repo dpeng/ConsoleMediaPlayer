5bf8e338f12fbad5ae24469f7eb0fe5ff7c49d35
dpeng <df.peng@hotmail.com>
2019-01-15 10:08:27 +0800

    add progressbar

---
+++ ConsoleMediaPlayer/ConsoleMediaPlayer.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+extern "C"
+{
+#include <ffplay/ffplay.h>
+#include <SDL/include/SDL_timer.h>
+#include <progressbar/progressbar.h>
+}
+
+
+progressbar *m_pProgressBar = NULL;
