9e74ff23ac129f10af20fde28615d69a65fc5892
dpeng <df.peng@hotmail.com>
2019-01-15 11:18:29 +0800

    add mutil-file support, and play next automatically

--- ConsoleMediaPlayer/ConsoleMediaPlayer.h
+++ ConsoleMediaPlayer/ConsoleMediaPlayer.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
 extern "C"
 {
 #include <ffplay/ffplay.h>
@@ -10,3 +14,10 @@ extern "C"
 
 
 progressbar *m_pProgressBar = NULL;
+vector<char*> filenameVector;
+int curPlayingIndex;
+SDL_TimerID progressBarTimerID = 0;
+
+
+int CMP_Play(void);
+int CMP_Stop(void);
